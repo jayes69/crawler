faabf36ee743c9ebb79ed1a31e9b71e2462125d4
Schneider Tobias <schneidert@net-up.net>
2020-01-10 16:45:06 +0100

    added 3 new pages for the crawler

--- crawl.rb
+++ crawl.rb
@@ -6,11 +6,12 @@ require 'mechanize'
 require 'mail'
 require 'diffy'
 require 'http-cookie'
+require 'fileutils'
 require_relative 'lib/eventbus.rb'
 
-def get_id
+def get_id(url)
   puts "Get Page Table"
-  page = Nokogiri::HTML(open("https://www.wikifolio.com/de/de/w/wf00esi893"))
+  page = Nokogiri::HTML(open(url))
   puts "Get wikifolioId"
   match = page.css('script')
   match.each do |m|
@@ -22,8 +23,17 @@ def get_id
   end
 end
 
-def get_xml(agent)
-  id = get_id
+def get_page_name(url)
+  puts "Get Page"
+  page = Nokogiri::HTML(open(url))
+  page_names = page.css('.js-desc-title').map {|div| div.child.text.strip}
+  page_name = page_names[0]
+  puts "Page name: #{page_name}"
+  return page_name
+end
+
+def get_xml(agent, url)
+  id = get_id(url)
   time = Time.now().to_i*1000
   page = agent.get("http://wikifolio.com/api/wikifolio/#{id}/tradehistory?page=0&pageSize=10&_=#{time}")
   html = agent.current_page.body
@@ -34,8 +44,8 @@ end
 def build_body(event)
   return if !event.is_a?(Hash)
   type = translate_type(event['type'])
-
-  body = "Name: #{event['name']}"                    if !event['name'].nil?
+  body = ""
+  body << "Name: #{event['name']}"                   if !event['name'].nil?
   body << " #{event['isin']}"                        if !event['isin'].nil?
   body << ", Typ: #{type}"                           if !event['type'].nil?
   body << ", Ausgeführt: #{event['date']}"           if !event['date'].nil?
@@ -43,6 +53,18 @@ def build_body(event)
   body << ", Gewichtung: #{event['weightage']}%"     if !event['weightage'].nil?
   body << ", Performance: #{event['performance']}%"  if !event['performance'].nil?
   return body
+end
+
+def build_subject(event)
+  return if !event.is_a?(Hash)
+  if !event['type'].nil?
+    type = translate_type(event['type']) 
+  else
+    type = ""
+  end
+  subject = event['page_name'] + " " + type
+  puts 
+  return subject
 end
 
 def translate_type(type)
@@ -89,17 +111,14 @@ eventbus.subscribe do |event|
     delivery_method :smtp, options
   end
   puts "Create new mail"
-  if !event['type'].nil?
-    type = translate_type(event['type']) 
-  else
-    type = ""
-  end
+  
   mail = Mail.deliver do
     from    "test@medicomit.de"
-    to      "tobias.schneider@net-up.de" #"js@net-up.de"
-    subject "Börsenbriefempfehlungen nutzen: " + type
+    to      "js@net-up.de"
+    subject build_subject(event)
     body    build_body(event)
   end
+
   puts "Send mail"
   mail.deliver!
 end
@@ -119,69 +138,99 @@ agent.get("https://www.wikifolio.com/dynamic/de/de/login/login?ReturnUrl=/de/de/
   puts "Login"
   agent.submit(folio_form)
 
-  #first time only
-  #xml = get_xml(agent)
-  #File.open('old.xml', "w") do |f|
-  #  f.write(xml)
-  #end
+  urls = {:stock =>     "https://www.wikifolio.com/de/de/w/wf00esi893",
+          :plat =>      "https://www.wikifolio.com/de/de/w/wfpt78pt78",
+          :grodival =>  "https://www.wikifolio.com/de/de/w/wf00smylel",
+          :hightech =>  "https://www.wikifolio.com/de/de/w/wf0stwtech"}
 
-  xml = get_xml(agent)
-  File.open('new.xml', "w") do |f|
-    f.write(xml)
-  end
+  urls.each do |name, url|
 
-  diff = Diffy::Diff.new('./old.xml', './new.xml', :source => 'files')
-  diff_s = diff.to_s(:html)
-  diff_a = diff.to_a
+    puts "Name: #{name}"
+    puts "URL: #{url}"
 
-  #write down diff
-  File.open('diff.xml', "w") do |f|
-    f.write(diff_s)
-  end
-
-  if (diff_s == '<div class="diff"></div>')
-    puts "NO NEW INFO " + Time.now.to_s
-  else
-    
-    info_hash = {}
-
-    diff_a.each do |line|
-      if line.start_with?('+') #new entries
-        puts line
-        if !(type = /OrderType>(.*?)</.match(line.to_s)).nil?
-          type = type[1]
-          info_hash['type'] = type
-        elsif !(name = /Name>(.*?)</.match(line.to_s)).nil?
-          name = name[1]
-          info_hash['name'] = name
-        elsif !(isin = /Isin>(.*?)</.match(line.to_s)).nil?
-          isin = isin[1]
-          info_hash['isin'] = isin
-        elsif !(date = /ExecutionDate>(.*?)</.match(line.to_s)).nil?
-          date = date[1]
-          date = DateTime.parse(date)
-          date = date.strftime("%d.%m.%Y %H:%M")
-          info_hash['date'] = date
-        elsif !(price = /ExecutionPrice>(.*?)</.match(line.to_s)).nil?
-          price = price[1]
-          info_hash['price'] = price
-        elsif !(weightage = /Weightage>(.*?)</.match(line.to_s)).nil?
-          weightage = weightage[1]
-          info_hash['weightage'] = (weightage.to_f*100).round(2)
-        elsif !(performance = /Performance>(.*?)</.match(line.to_s)).nil?
-          performance = performance[1]
-          info_hash['performance'] = (performance.to_f*100).round(2)
-        end
+    #if new entry
+    pn = "./pages/#{name}/new.xml"
+    if !File.file?(pn)
+      dirname = File.dirname(pn)
+      unless File.directory?(dirname)
+        FileUtils.mkdir_p(dirname)
+      end
+      xml = get_xml(agent, url)
+      File.open("./pages/#{name}/old.xml", "w") do |f|
+        f.write(xml)
+      end
+      File.open("./pages/#{name}/new.xml", "w") do |f|
+        f.write(xml)
+      end
+      File.open("./pages/#{name}/diff.xml", "w") do |f|
+        f.write('<div class="diff"></div>')
       end
     end
 
-    eventbus.publish(info_hash) #send Email
-
-    #new data is saved as old for comparison
-    File.open('old.xml', "w") do |f|
+    xml = get_xml(agent, url)
+    File.open("./pages/#{name}/new.xml", "w") do |f|
       f.write(xml)
     end
 
-    puts "NEW INFO " + Time.now.to_s
+    puts "Open and compare files"
+    diff = Diffy::Diff.new("./pages/#{name}/old.xml", "./pages/#{name}/new.xml", :source => "files")
+    diff_s = diff.to_s(:html)
+    diff_a = diff.to_a
+
+    #write down diff
+    File.open("./pages/#{name}/diff.xml", "w") do |f|
+      f.write(diff_s)
+    end
+
+    if (diff_s == '<div class="diff"></div>')
+      puts "NO NEW INFO " + Time.now.to_s
+    else
+      
+      info_hash = {}
+      page_name = get_page_name(url)
+      info_hash['page_name'] = page_name
+
+      diff_a.each do |line|
+        if line.start_with?('+') #new entries
+          puts line
+          if !(type = /OrderType>(.*?)</.match(line.to_s)).nil?
+            type = type[1]
+            info_hash['type'] = type
+          elsif !(trade_name = /Name>(.*?)</.match(line.to_s)).nil?
+            trade_name = trade_name[1]
+            info_hash['name'] = trade_name
+          elsif !(isin = /Isin>(.*?)</.match(line.to_s)).nil?
+            isin = isin[1]
+            info_hash['isin'] = isin
+          elsif !(date = /ExecutionDate>(.*?)</.match(line.to_s)).nil?
+            date = date[1]
+            date = DateTime.parse(date)
+            date = date.strftime("%d.%m.%Y %H:%M")
+            info_hash['date'] = date
+          elsif !(price = /ExecutionPrice>(.*?)</.match(line.to_s)).nil?
+            price = price[1]
+            info_hash['price'] = price
+          elsif !(weightage = /Weightage>(.*?)</.match(line.to_s)).nil?
+            weightage = weightage[1]
+            info_hash['weightage'] = (weightage.to_f*100).round(2)
+          elsif !(performance = /Performance>(.*?)</.match(line.to_s)).nil?
+            performance = performance[1]
+            info_hash['performance'] = (performance.to_f*100).round(2)
+          end
+        end
+      end
+
+      puts "Info Hash:"
+      puts info_hash
+
+      eventbus.publish(info_hash) #send Email
+
+      #new data is saved as old for comparison
+      File.open("./pages/#{name}/old.xml", "w") do |f|
+        f.write(xml)
+      end
+      puts "NEW INFO " + Time.now.to_s
+
+    end
   end
 end
