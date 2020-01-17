d71c6270cc195a5b8ee7d7c97de7ecbdf0e03d42
Schneider Tobias <schneidert@net-up.net>
2020-01-17 16:58:25 +0100

    added logger

--- crawl.rb
+++ crawl.rb
@@ -8,27 +8,30 @@ require 'diffy'
 require 'http-cookie'
 require 'fileutils'
 require_relative 'lib/eventbus.rb'
+require 'logger'
+
+logger = Logger.new('logfile.log', 1, 1024000)
+logger.level = Logger::DEBUG
+
 
 def get_id(url)
-  puts "Get Page Table"
+  #Get Page Table
   page = Nokogiri::HTML(open(url))
-  puts "Get wikifolioId"
+  #Get wikifolioId
   match = page.css('script')
   match.each do |m|
     if !(id = /wikifolioId: "(.*?)"/.match(m.to_s)).nil?
       id = id[1]
-      puts "ID: #{id}"
       return id
     end
   end
 end
 
 def get_page_name(url)
-  puts "Get Page"
+  #Get Page
   page = Nokogiri::HTML(open(url))
   page_names = page.css('.js-desc-title').map {|div| div.child.text.strip}
   page_name = page_names[0]
-  puts "Page name: #{page_name}"
   return page_name
 end
 
@@ -63,7 +66,6 @@ def build_subject(event)
     type = ""
   end
   subject = event['page_name'] + " " + type
-  puts 
   return subject
 end
 
@@ -94,8 +96,7 @@ I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
 eventbus = Eventbus.new
 
 eventbus.subscribe do |event|
-  puts "Subscribe event: #{event}"
-  puts "Set Mail options"
+  #Set Mail options
   options = { 
     :address              => "mail.medicomit.de",
     :port                 => 25,
@@ -106,21 +107,30 @@ eventbus.subscribe do |event|
     :enable_starttls_auto => false,
     :openssl_verify_mode  => 'none'
   }
-  puts "Set Mail defaults"
+  #Set Mail defaults
   Mail.defaults do
     delivery_method :smtp, options
   end
-  puts "Create new mail"
+  #Create new mail
   
-  mail = Mail.deliver do
-    from    "test@medicomit.de"
-    to      "tobias.schneider@net-up.de"
-    subject build_subject(event)
-    body    build_body(event)
-  end
+  email_subject = build_subject(event)
+  email_body = build_body(event)
 
-  puts "Send mail"
-  mail.deliver!
+  if !email_body.empty?
+    mail = Mail.deliver do
+      from    "test@medicomit.de"
+      to      "js@net-up.de"
+      subject email_subject
+      body    email_body
+    end
+
+    #Send mail
+    logger.debug("Sending Email: #{email_subject} with body: \n #{email_body}")
+    mail.deliver!
+  else
+    #Email would have been empty
+    logger.debug("Email would have been empty")
+  end
 end
 
 agent = Mechanize.new
@@ -129,13 +139,13 @@ agent.user_agent_alias = 'Windows Chrome'
 time = Time.now().to_i*1000
 
 agent.get("https://www.wikifolio.com/dynamic/de/de/login/login?ReturnUrl=/de/de/home-e&_=#{time}") do |page|
-  puts "Go to Login"
+  #Go to Login
   folio_form = page.form
-  puts "Setting email"
+  #Setting email
   folio_form.Username = 'support@net-up.de'
-  puts "Setting password"
+  #Setting password
   folio_form.Password = 'Kj23d9MwcQ7rg'
-  puts "Login"
+  #Login
   agent.submit(folio_form)
 
   urls = {:stock =>     "https://www.wikifolio.com/de/de/w/wf00esi893",
@@ -144,9 +154,6 @@ agent.get("https://www.wikifolio.com/dynamic/de/de/login/login?ReturnUrl=/de/de/
           :hightech =>  "https://www.wikifolio.com/de/de/w/wf0stwtech"}
 
   urls.each do |name, url|
-
-    puts "Name: #{name}"
-    puts "URL: #{url}"
 
     #if new entry
     pn = "./pages/#{name}/new.xml"
@@ -172,15 +179,13 @@ agent.get("https://www.wikifolio.com/dynamic/de/de/login/login?ReturnUrl=/de/de/
       f.write(xml)
     end
 
-    puts "Open and compare files"
+    #Open and compare files
     diff = Diffy::Diff.new("./pages/#{name}/old.xml", "./pages/#{name}/new.xml", :source => "files")
     diff_s = diff.to_s(:html)
     diff_a = diff.to_a
 
     if (diff_s == '<div class="diff"></div>')
-      puts "NO NEW INFO " + Time.now.to_s
     else
-      
       #write down diff
       File.open("./pages/#{name}/diff.xml", "w") do |f|
         f.write(diff_s)
@@ -189,39 +194,44 @@ agent.get("https://www.wikifolio.com/dynamic/de/de/login/login?ReturnUrl=/de/de/
       info_hash = {}
       page_name = get_page_name(url)
       info_hash['page_name'] = page_name
+      logger.debug("NEW INFO for #{page_name} " + Time.now.to_s)
+      logger.debug("Info Hash:")
 
       diff_a.each do |line|
         if line.start_with?('+') #new entries
-          puts line
           if !(type = /OrderType>(.*?)</.match(line.to_s)).nil?
             type = type[1]
             info_hash['type'] = type
+            logger.debug("Type: #{type}")
           elsif !(trade_name = /Name>(.*?)</.match(line.to_s)).nil?
             trade_name = trade_name[1]
             info_hash['name'] = trade_name
+            logger.debug("Name: #{trade_name}")
           elsif !(isin = /Isin>(.*?)</.match(line.to_s)).nil?
             isin = isin[1]
             info_hash['isin'] = isin
+            logger.debug("Isin: #{isin}")
           elsif !(date = /ExecutionDate>(.*?)</.match(line.to_s)).nil?
             date = date[1]
             date = DateTime.parse(date)
             date = date.strftime("%d.%m.%Y %H:%M")
             info_hash['date'] = date
+            logger.debug("Date: #{date}")
           elsif !(price = /ExecutionPrice>(.*?)</.match(line.to_s)).nil?
             price = price[1]
             info_hash['price'] = price
+            logger.debug("Price: #{price}")
           elsif !(weightage = /Weightage>(.*?)</.match(line.to_s)).nil?
             weightage = weightage[1]
             info_hash['weightage'] = (weightage.to_f*100).round(2)
+            logger.debug("Weightage: #{info_hash['weightage']}%")
           elsif !(performance = /Performance>(.*?)</.match(line.to_s)).nil?
             performance = performance[1]
             info_hash['performance'] = (performance.to_f*100).round(2)
+            logger.debug("Performance: #{info_hash['performance']}%")
           end
         end
       end
-
-      puts "Info Hash:"
-      puts info_hash
 
       eventbus.publish(info_hash) #send Email
 
@@ -229,8 +239,6 @@ agent.get("https://www.wikifolio.com/dynamic/de/de/login/login?ReturnUrl=/de/de/
       File.open("./pages/#{name}/old.xml", "w") do |f|
         f.write(xml)
       end
-      puts "NEW INFO " + Time.now.to_s
-
     end
   end
 end
